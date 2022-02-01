3c4e3b7a0dbdaafb0a687021935f5cddb0bfc6a1
priscilla ma <priscillama621@gmail.com>
2022-02-01 15:20:16 -0500

    removed background color from css, indented and organized js, added my coinapi key as variable that we will change later just wanted it to be there hehe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// POKEMON
+
 // Getting the first 250 cards
 fetch('https://api.pokemontcg.io/v2/cards',{
     headers:{
@@ -5,39 +7,45 @@ fetch('https://api.pokemontcg.io/v2/cards',{
         "X-Api-Key": "e8bdc35b-8907-45b0-b1c1-eb5d3cd116a6"
     }
 })
-//turning into the json object
-.then(function(response){
-    return response.json()
-})
-//drilling down to the first entry
-.then(function(data){
-    console.log(data.data[0]);
-})
+// Turning into the json object
+    .then(function(response){
+        return response.json()
+    })
+// Drilling down to the first entry
+    .then(function(data){
+        console.log(data.data[0]);
+    })
 
 
+
+    
+// COINAPI
+
+var priscillasApi = "612EED13-BBA7-4E82-AF79-78955F897E68" // :)
 
 
 //              0    1       2
-//Add more cryptos for later
-var cryptoTickers =['BTC', 'ETH', 'USDT']
+
+// Add more cryptos for later
+var cryptoTickers = ['BTC', 'ETH', 'USDT']
 // var cryptoInfo ={
 //     cryptoID: data.asset_id_quote,
 //     cryptoRate: data.rate
 //     // data.rate + ":" + data.asset_id_quote
 // }
-var cryptoInfo ={}
-for(var i=0; i<cryptoTickers.length; i++){
+var cryptoInfo = {}
+for(var i = 0; i < cryptoTickers.length; i++){
 //console.log(i);
-fetch(	'https://rest.coinapi.io/v1/exchangerate/USD/'+cryptoTickers[i]+'?apikey=EE0B3E38-CFD9-4BEE-8F5D-BF82AE6DD7BF')
-.then(function(response){
-    return response.json()
-})
-.then(function(data){
-   // console.log(data.rate); 
-    //console.log(data.asset_id_quote);
-    cryptoInfo[data.asset_id_quote]=data.rate
-})
-sleep(500)
+    fetch('https://rest.coinapi.io/v1/exchangerate/USD/' + cryptoTickers[i] + '?apikey=EE0B3E38-CFD9-4BEE-8F5D-BF82AE6DD7BF')
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            //console.log(data.rate);
+            //console.log(data.asset_id_quote);
+            cryptoInfo[data.asset_id_quote] = data.rate
+        })
+    sleep(500)
 };
 console.log(cryptoInfo);
 
@@ -58,12 +66,12 @@ console.log(cryptoInfo);
 //     console.log(data);
 // })
 
-//https://www.sitepoint.com/delay-sleep-pause-wait/
-function sleep(milliseconds) {
+
+// credit: https://www.sitepoint.com/delay-sleep-pause-wait/
+function sleep(milliseconds){
     const date = Date.now();
     let currentDate = null;
     do {
       currentDate = Date.now();
     } while (currentDate - date < milliseconds);
-  }
-  
+}

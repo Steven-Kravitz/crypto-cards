45b3e571709bce7e72c18d89108d3ad2340efa57
KreshsDesktop <ckresh99@gmail.com>
2022-02-01 12:08:37 -0500

    testing the fetch command for the APIs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Getting teh first 250 cards
+// Getting the first 250 cards
 fetch('https://api.pokemontcg.io/v2/cards',{
     headers:{
         "Content-Type":"application/json",
@@ -13,3 +13,15 @@ fetch('https://api.pokemontcg.io/v2/cards',{
 .then(function(data){
     console.log(data.data[0]);
 })
+
+var crypto =['BTC', 'ETH', 'UDST']
+for(var i=0; i<crypto.length; i++){
+    console.log(i)
+fetch(	'https://rest.coinapi.io/v1/exchangerate/'+crypto[i]+'/USD?apikey=EE0B3E38-CFD9-4BEE-8F5D-BF82AE6DD7BF')
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data);
+})
+}

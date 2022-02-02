1c263698e602568f278005cb9bb30abec11a1661
Steven Kravitz <Steven.Kravitz95@gmail.com>
2022-02-02 10:55:50 -0500

    Changed api pull to query based input from user template

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // POKEMON
-
+var userInput='pikachu';
 // Getting the first 250 cards
-fetch('https://api.pokemontcg.io/v2/cards',{
+fetch(`https://api.pokemontcg.io/v2/cards?q=name:${userInput}`,{
     headers:{
         "Content-Type":"application/json",
         "X-Api-Key": "e8bdc35b-8907-45b0-b1c1-eb5d3cd116a6"
@@ -14,17 +14,22 @@ fetch('https://api.pokemontcg.io/v2/cards',{
 // Drilling down to the first entry
     .then(function(data){
         console.log(data.data[0]);
+        console.log(data.data[0].name)
+        console.log(data.data[0].images)
+
     })
 
-
+//Attributes we need from tcgpokemonapi. tcgplayer (pricing), name, images, 
 
     
 // COINAPI
-
+// Add if statement swapping API keys at limit
 var priscillasApi = "612EED13-BBA7-4E82-AF79-78955F897E68" // :)
 var stevensAPI = "865395AE-4F4E-48D5-A825-65822194FDE7" // :D
 var jenAPI = "2FEC7249-7A76-4CEB-AB8B-EEEB43C6F19E" // ^-^
+var chrisAPI = "EE0B3E38-CFD9-4BEE-8F5D-BF82AE6DD7BF" // O.O
 
+var coinAPIKeys = [priscillasApi, stevensAPI, jenAPI, chrisAPI]
 //              0    1       2
 
 // Add more cryptos for later
@@ -34,45 +39,30 @@ var cryptoTickers = ['BTC', 'ETH', 'USDT']
 //     cryptoRate: data.rate
 //     // data.rate + ":" + data.asset_id_quote
 // }
-var cryptoInfo = {}
-for(var i = 0; i < cryptoTickers.length; i++){
-//console.log(i);
-    fetch('https://rest.coinapi.io/v1/exchangerate/USD/' + cryptoTickers[i] + '?apikey=EE0B3E38-CFD9-4BEE-8F5D-BF82AE6DD7BF')
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(data){
-            //console.log(data.rate);
-            //console.log(data.asset_id_quote);
-            cryptoInfo[data.asset_id_quote] = data.rate
-        })
-    sleep(500)
-};
-console.log(cryptoInfo);
 
 
-// fetch(	'https://rest.coinapi.io/v1/exchangerate/USD/ETH?apikey=EE0B3E38-CFD9-4BEE-8F5D-BF82AE6DD7BF')
-// .then(function(response){
-//     return response.json()
-// })
-// .then(function(data){
-//     console.log(data);
-// })
-// sleep(500)
-// fetch(	'https://rest.coinapi.io/v1/exchangerate/USD/USDT?apikey=EE0B3E38-CFD9-4BEE-8F5D-BF82AE6DD7BF')
-// .then(function(response){
-//     return response.json()
-// })
-// .then(function(data){
-//     console.log(data);
-// })
 
+// var cryptoInfo = {}
+// for(var i = 0; i < cryptoTickers.length; i++){
+// //console.log(i);
+//     fetch('https://rest.coinapi.io/v1/exchangerate/USD/' + cryptoTickers[i] + '?apikey=' + coinAPIKeys[0])
+//         .then(function(response){
+//             return response.json();
+//         })
+//         .then(function(data){
+//             //console.log(data.rate);
+//             //console.log(data.asset_id_quote);
+//             cryptoInfo[data.asset_id_quote] = data.rate
+//         })
+//     sleep(500)
+// };
+// console.log(cryptoInfo);
 
-// credit: https://www.sitepoint.com/delay-sleep-pause-wait/
-function sleep(milliseconds){
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-}
+// // credit: https://www.sitepoint.com/delay-sleep-pause-wait/
+// function sleep(milliseconds){
+//     const date = Date.now();
+//     let currentDate = null;
+//     do {
+//       currentDate = Date.now();
+//     } while (currentDate - date < milliseconds);
+// }

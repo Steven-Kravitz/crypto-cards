441ee8ec0b53f2804a6ab10a7e069d3f4eb3fbb4
priscilla ma <priscillama621@gmail.com>
2022-02-04 13:41:43 -0500

    adding comments js file to help organize and manage our project.

---
+++ assets/js/comments.js
@@ -0,0 +1,13 @@
+// https://api.pokemontcg.io/v2/cards
+
+// New pages:
+// 1. Search page/results
+// 2. My Deck
+// 3. Pokemon by set
+// 4. Pokemon by type
+
+// HAVE TO ADD SEARCH BAR FUNCTION ON THE RESULTS PAGE.💟
+
+// ** When you click on tab in nav and go to the page, should be highlighted in the nav to let user know which page they’re on.
+
+// any more comments/to-do u guys have:
